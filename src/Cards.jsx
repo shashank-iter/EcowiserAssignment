b802a3fee5a0825ab22c9097ba5130462837d97a
shashank-iter <pandeyshashank812@gmail.com>
2023-08-27 13:39:23 +0530

    minor fixes

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -15,50 +15,7 @@ import OverlayEdit from "./OverlayEdit";
 import supabase from "./supabaseClient";
 import swal from "sweetalert";
 
-const actions = [
-  {
-    title: "Request time off",
-    href: "#",
-    icon: ClockIcon,
-    iconForeground: "text-teal-700",
-    iconBackground: "bg-teal-50",
-  },
-  {
-    title: "Benefits",
-    href: "#",
-    icon: PencilSquareIcon,
-    iconForeground: "text-purple-700",
-    iconBackground: "bg-purple-50",
-  },
-  {
-    title: "Schedule a one-on-one",
-    href: "#",
-    icon: UsersIcon,
-    iconForeground: "text-sky-700",
-    iconBackground: "bg-sky-50",
-  },
-  {
-    title: "Payroll",
-    href: "#",
-    icon: BanknotesIcon,
-    iconForeground: "text-yellow-700",
-    iconBackground: "bg-yellow-50",
-  },
-  {
-    title: "Submit an expense",
-    href: "#",
-    icon: ReceiptRefundIcon,
-    iconForeground: "text-rose-700",
-    iconBackground: "bg-rose-50",
-  },
-  {
-    title: "Training",
-    href: "#",
-    icon: AcademicCapIcon,
-    iconForeground: "text-indigo-700",
-    iconBackground: "bg-indigo-50",
-  },
-];
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
